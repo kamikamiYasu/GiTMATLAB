25acf3043ce83e0a3b0d50e3ff1e740c69ea849f
HomePC <nextnexus7@gmail.com>
2018-09-30 21:38:14 +0900

    コメント追加

--- PlotGraf.m
+++ PlotGraf.m
@@ -1,8 +1,12 @@
+%入力に使うデータセルを指定
 inputData = plotDataTable2
+%グラフの上書き設定
 hold on 
+%x，y軸のラベル設定
 xlabel('$$\varepsilon$$','Interpreter','Latex','FontSize',36);
 ylabel('$$R_{sec}$$','Interpreter', 'Latex','FontSize',36);
 [row,col] = size(inputData);
+%x軸にセルの１列目を、それ以外をy軸にそれぞれ使用してグラフを作成
     plot(inputData{:,1},inputData{:,2:col},'-');
     legend(inputData.Properties.VariableNames{2:col});
 
---
+++ savedata.m
@@ -0,0 +1,5 @@
+%終了前にはこれを実行！
+
+save('plotDataTable.mat','plotDataTable')
+save('havedelta.mat','havedelta')
+save('matrixstruct.mat','matrixstruct')
