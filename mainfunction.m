c15dae69fb0cecd6772c3ac04c385604598c42e7
DESKTOP-R7EMID0\gear3353 <nextnexus7@gmail.com>
2018-12-24 17:01:53 +0900

    no message

--- mainfunction.m
+++ mainfunction.m
@@ -1,9 +1,8 @@
 %デルタの値を計算
-deltadata = idealdelta%deltatest(matrixstruct,havedelta,'ini');
+% deltadata = idealdelta%deltatest(matrixstruct,havedelta,'ini');
 %Rsecの値を計算(!引数に使用するものはdeltatestと同じもの!)
-plotData = setEvalutionData(matrixstruct,deltadata,0.01);
+% plotData = setEvalutionData(matrixstruct,deltadata,0.01);
 %グラフを書く
-PlotGraf(plotData)
+PlotGraf(plotDataTable)
 %計算したデータを格納(!代入するのはdeltatestなどで使用した変数に!)
 %havedelta = deltadata
-plotIdealTable = plotData
