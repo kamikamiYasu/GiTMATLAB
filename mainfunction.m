52f0452484e4da188c425b4ba539479fc76976f8
LabPC <watosonjon@gmail.com>
2019-01-06 20:58:10 +0900

    getMaxMatrixの計算が間違っていたので変更

--- mainfunction.m
+++ mainfunction.m
@@ -1,11 +1,10 @@
 %HarfRatedelta = struct('name',[],'delta',[])
 %デルタの値を計算
-%deltadata = deltatest(HarfRateMatrix,HarfRatedelta,'ini');
+deltadata = deltatest(HarfRateMatrix,HarfRatedelta,'ini');
 %Rsecの値を計算(!引数に使用するものはdeltatestと同じもの!)
-HarfRatedelta(1).delta = deltadata
 plotData = setEvalutionData(HarfRateMatrix,HarfRatedelta,0.01);
 %グラフを書く
 PlotGraf(plotData)
 %計算したデータを格納(!代入するのはdeltatestなどで使用した変数に!)
-%HarfRatedelta = deltadata
+HarfRatedelta = deltadata
 plotHarfRate = plotData
