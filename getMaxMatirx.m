4a9060ae1ee6769ebd4f34370de8043b4ff9d45c
LabPC <watosonjon@gmail.com>
2019-01-06 21:14:10 +0900

    またまたプログラムが間違っていた(最大の情報漏えい量を求めてた)ので修正

--- getMaxMatirx.m
+++ getMaxMatirx.m
@@ -3,7 +3,7 @@ m = 5
 %ïÑçÜí∑
 n = 10
 MatrixSource =zeros(m,(2^m -1))
-MatrixIndex = [1:(2^(m+1) - 1)];
+MatrixIndex = [1:(2^m+ - 1)];
 for i = 1:(2^m - 1)
 buff = fliplr(de2bi(i,m))
 
@@ -12,16 +12,16 @@ end
 pickupIndex = nchoosek(MatrixIndex,m)
 tempmatrix = zeros(m,n);
 tempdelta = zeros(1,n);
-maxmatrix = tempmatrix;
-maxdelta = tempdelta;
+minmatrix = tempmatrix;
+mindelta = ones(1,n) * m;
 buff = eye(m);
 for i = 1:length(pickupIndex)
-        tempmatrix = horzcat(MatrixSource(:,pickupIndex(i,:)),buff)
+        tempmatrix = horzcat(MatrixSource(:,pickupIndex(i,:)),buff);
         if gfrank(tempmatrix) == m
             tempdelta = deltatest(tempmatrix,tempdelta,'ini');
-            if maxdelta <= tempdelta
-                maxdelta = tempdelta;
-                maxmatrix = tempmatrix;
+            if tempdelta <= mindelta 
+                mindelta = tempdelta;
+                minmatrix = tempmatrix;
             end
         end
     
