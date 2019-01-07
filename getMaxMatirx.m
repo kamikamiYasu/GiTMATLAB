6a0a32cc3bd518516f3e4968cc46ef60b21f8176
LabPC <watosonjon@gmail.com>
2019-01-07 13:55:03 +0900

    toriaezu

--- getMaxMatirx.m
+++ getMaxMatirx.m
@@ -3,7 +3,7 @@ m = 5
 %•„†’·
 n = 10
 MatrixSource =zeros(m,(2^m -1))
-MatrixIndex = [1:(2^m+ - 1)];
+MatrixIndex = [1:(2^m - 1)];
 for i = 1:(2^m - 1)
 buff = fliplr(de2bi(i,m))
 
--- mainfunction.m
+++ mainfunction.m
@@ -1,10 +1,10 @@
 %HarfRatedelta = struct('name',[],'delta',[])
 %デルタの値を計算
-deltadata = HarfRateIdealdelta%deltatest(HarfRateMatrix,HarfRatedelta,'ini');
+deltadata = HarfRatedelta%deltatest(HarfRateMatrix,HarfRatedelta,'ini');
 %Rsecの値を計算(!引数に使用するものはdeltatestと同じもの!)
 plotData = setEvalutionData(HarfRateMatrix,deltadata,0.01);
 %グラフを書く
 PlotGraf(plotData)
 %計算したデータを格納(!代入するのはdeltatestなどで使用した変数に!)
 %HarfRatedelta = deltadata
-plotIdealHarfRate = plotData
+plotHarfRate = plotData
