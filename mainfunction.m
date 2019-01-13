d84e6675ae5b6437b0ac98848b5a506980d338cb
LabPC <watosonjon@gmail.com>
2019-01-13 18:26:39 +0900

    最適なLin94とそれを拡張したExLin94のデータを追加。それに伴いcalcIntegralを１つのてーぶるデータが１列でも動くように改造

--- mainfunction.m
+++ mainfunction.m
@@ -1,10 +1,10 @@
 %HarfRatedelta = struct('name',[],'delta',[])
 %デルタの値を計算
-deltadata = calcIdealdelta(FTRateMatrix)%deltatest(FTRateMatrix,FTRatedelta,'ini');
+deltadata = ExLin94delta%deltatest(minmatrix,mindelta,'ini');
 %Rsecの値を計算(!引数に使用するものはdeltatestと同じもの!)
-plotData = setEvalutionData(FTRateMatrix,deltadata,0.01);
+plotData = setEvalutionData(ExLin94,deltadata,0.01);
 %グラフを書く
-PlotGraf(plotData)
+%PlotGraf(plotData)
 %計算したデータを格納(!代入するのはdeltatestなどで使用した変数に!)
-FTRateIdealdelta = deltadata
-plotFTIdealRate = plotData
+%FTRateIdealdelta = deltadata
+%plotFTIdealRate = plotData
