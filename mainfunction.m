5278362e26b3066760092e3d1faeaf3aec059fba
HomePC <nextnexus7@gmail.com>
2019-01-31 22:43:58 +0900

    lll

--- mainfunction.m
+++ mainfunction.m
@@ -1,10 +1,10 @@
-%HarfRatedelta = struct('name',[],'delta',[])
+%deltadata = struct('name',[],'delta',[])
 %デルタの値を計算
-deltadata =deltatest(minmatrix,mindelta,'ini');
+deltadata = calcIdealdelta(matrixstruct)%deltatest(matrixstruct,deltadata,'ini');
 %Rsecの値を計算(!引数に使用するものはdeltatestと同じもの!)
-%plotData = setEvalutionData(ExLin94,deltadata,0.01);
+plotData = setEvalutionData(matrixstruct,deltadata,0.01);
 %グラフを書く
 %PlotGraf(plotData)
 %計算したデータを格納(!代入するのはdeltatestなどで使用した変数に!)
 %FTRateIdealdelta = deltadata
-%plotFTIdealRate = plotData
+plotIdealTable = plotData
