9c9a916c49c75c863fc883d6cadf2b70f2d26685
HomePC <nextnexus7@gmail.com>
2019-02-04 00:26:42 +0900

    日曜の作業分

--- mainfunction.m
+++ mainfunction.m
@@ -1,10 +1,10 @@
-%deltadata = struct('name',[],'delta',[])
+deltadata = struct('name',[],'delta',[])
 %デルタの値を計算
-deltadata = calcIdealdelta(matrixstruct)%deltatest(matrixstruct,deltadata,'ini');
+deltadata = deltatest(matrixstruct,deltadata,'ini');
 %Rsecの値を計算(!引数に使用するものはdeltatestと同じもの!)
 plotData = setEvalutionData(matrixstruct,deltadata,0.01);
 %グラフを書く
 PlotGraf(plotData)
 %計算したデータを格納(!代入するのはdeltatestなどで使用した変数に!)
 %FTRateIdealdelta = deltadata
-plotIdealTable = plotData
+plotDataTable = plotData
